77a51e9dda7ccc462d77bcd9769509e12d691795
bopo <ibopo@126.com>
2018-12-21 13:41:01 +0800

    update commit

--- pyalgotrade_mootdx/common.py
+++ pyalgotrade_mootdx/common.py
@@ -1,33 +1,20 @@
-# MooQuant MooTDX module
+# -*- coding: utf-8 -*-
+# PyAlgoTrade mootdx module
 #
-# Copyright 2011-2015 Gabriel Martin Becedillas Ruiz
+# Copyright 2017 bopo.wang<ibopo@126.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#   http://www.apache.org/licenses/LICENSE-2.0
+#    http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-# Modified from MooQuant mootdx and Xignite modules
 
 """
-.. moduleauthor:: Mikko Gozalo <mikgozalo@gmail.com>
+.. moduleauthor:: bopo.wang <ibopo@126.com>
 """
-
-import pyalgotrade.logger
-from pyalgotrade import broker
-
-
-btc_symbol = "600036"
-logger = pyalgotrade.logger.getLogger("mootdx")
-
-
-class BTCTraits(broker.InstrumentTraits):
-    def roundQuantity(self, quantity):
-        return round(quantity, 8)
